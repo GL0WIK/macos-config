e1b4a0f78e6c681ddeb5571a7746dd4f30560818
Rémi MONTAGUT <remi@MacBook-Pro-de-Remi-3.local>
2025-02-14 23:55:14 +0100

    .

--- setup.sh
+++ setup.sh
@@ -18,12 +18,12 @@ echo "📦 Installation de Git et Ansible..."
 brew install git ansible stow
 
 # Cloner le repo si nécessaire
-if [ ! -d "MACOS-CONFIG" ]; then
+if [ ! -d "macos-config" ]; then
     git clone https://github.com/GL0WIK/macos-config.git
 fi
 
 # Aller dans le dossier
-cd MACOS-CONFIG
+cd macos-config
 
 # 3️⃣ Lancer Ansible pour configurer le Mac
 echo "⚙️ Lancement d'Ansible pour appliquer la configuration..."
